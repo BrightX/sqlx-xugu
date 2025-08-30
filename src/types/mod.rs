4db153b3755815e54a6b95552bdf707ab934190f
Bright Xu <BrightXu666@qq.com>
2025-08-30 09:22:24 +0800

    feat: 日期时间 chrono 类型支持

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -5,6 +5,13 @@ mod int;
 mod str;
 mod text;
 mod uint;
+
+/// The number of microseconds per in days.
+#[allow(dead_code)]
+const MICROS_PER_DAY: i64 = 86_400_000_000;
+/// The number of microseconds per in days.
+const MICROS_PER_DAY_F: f64 = 86_400_000_000.0;
+
 #[cfg(feature = "json")]
 mod json;
 
