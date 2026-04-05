55ad4b22b534c6fdcc0c98fbae3808a035c220ea
Bright Xu <BrightXu666@qq.com>
2026-04-05 20:15:26 +0800

    refactor: 重构 ping 命令

--- src/protocol/text/ping.rs
+++ src/protocol/text/ping.rs
@@ -7,9 +7,8 @@ pub(crate) struct Ping;
 
 impl ProtocolEncode<'_, ()> for Ping {
     fn encode_with(&self, buf: &mut Vec<u8>, _: ()) -> Result<(), Error> {
-        // 选择设置语句是因为隔离级别的set语句对服务器无影响，且命令执行结果接收字节少
-        // TRANSACTION_READ_UNCOMMITTED
-        const SQL_CMD: &str = "SET SESSION_ISO_LEVEL TO 1";
+        // 选择 PL/SQL 语句是因为 null 语句对服务器无影响，且命令执行结果接收字节少
+        const SQL_CMD: &str = "begin/* SQLx ping */null;end";
         encode_command0(buf, SQL_CMD);
         Ok(())
     }
